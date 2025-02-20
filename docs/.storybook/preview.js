3ef1ed42b466d909bc8974b9e324a80b4e08756e
Emre Şahin <doguruzgar@gmail.com>
2025-02-20 20:39:00 +0300

    Add Google Font and global styles to Storybook, and update button styles

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -1,5 +1,23 @@
 import '../../packages/ui/src/lib/styles/app.scss'
 
+// Add Google Font link
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
+// Add global styles for Storybook
+const customFontStyles = `
+  .storybook-root, .sb-story {
+    font-family: "Nunito Sans",-apple-system,".SFNSText-Regular","San Francisco",BlinkMacSystemFont,"Segoe UI","Helvetica Neue",Helvetica,Arial,sans-serif;
+  }
+`;
+
+const style = document.createElement('style');
+style.innerHTML = customFontStyles;
+document.head.appendChild(style);
+
+
 /** @type { import('@storybook/svelte').Preview } */
 const preview = {
   parameters: {
