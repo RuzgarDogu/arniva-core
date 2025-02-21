04dbb06446f131bbe893e10758ec2cf896bba3b5
Emre Şahin <doguruzgar@gmail.com>
2025-02-21 10:14:21 +0300

    Update brand logo URL in Storybook theme

--- docs/.storybook/arnivatheme.js
+++ docs/.storybook/arnivatheme.js
@@ -4,6 +4,6 @@ export default create({
   base: 'light',
   brandTitle: 'Arniva Docs',
   brandUrl: 'https://arniva.com.tr',
-  brandImage: '/arniva_docs_logo_test_01.png',
+  brandImage: 'https://i.imgur.com/ixkDWCe.png',
   brandTarget: '_self',
 });
