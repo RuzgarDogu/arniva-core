d779ecce22645a5f6fdf74d575bbdff29636980a
Emre Şahin <doguruzgar@gmail.com>
2025-02-21 10:07:15 +0300

    Update brand logo path in Storybook theme and add new logo asset

--- docs/.storybook/arnivatheme.js
+++ docs/.storybook/arnivatheme.js
@@ -4,6 +4,6 @@ export default create({
   base: 'light',
   brandTitle: 'Arniva Docs',
   brandUrl: 'https://arniva.com.tr',
-  brandImage: '../src/stories/assets/arniva_docs_logo_test_01.png',
+  brandImage: '/arniva_docs_logo_test_01.png',
   brandTarget: '_self',
 });
