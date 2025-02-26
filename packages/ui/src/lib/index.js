5ac4d1d990e4c5a85ff68c780de4c90714d75910
Emre Şahin <doguruzgar@gmail.com>
2025-02-26 10:49:26 +0300

    Update styles and scripts: rename app.scss to app.css, add build:styles script, and bump version to 0.0.4

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -1,6 +1,5 @@
 // Reexport your entry components here
-// Reexport your entry components here
-import './styles/app.scss'; // Ensures styles are included in the final build
+import './styles/app.css'; // Ensures styles are included in the final build
 export * from './general';
 export * from './layout';
 export * from './nav';
