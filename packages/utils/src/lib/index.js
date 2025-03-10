b202348f5996abd19e43a37e68c17c5e3619905c
Emre Şahin <doguruzgar@gmail.com>
2025-03-10 12:34:37 +0300

    Bump version to 0.0.12; update styles import from app.scss to app.css in utils and from app.css to app.scss in ui

--- packages/utils/src/lib/index.js
+++ packages/utils/src/lib/index.js
@@ -1,4 +1,4 @@
 // Reexport your entry components here
-import './styles/app.scss';
+import './styles/app.css';
 export { hello } from './test.svelte.js';
 export { default as toast } from './toast.svelte.js';
