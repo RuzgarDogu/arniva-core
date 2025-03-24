801795f565d4083d00d2ed21208fe938ae9cf058
Emre Şahin <doguruzgar@gmail.com>
2025-03-24 16:26:41 +0300

    Bump utils package version to 0.0.15; update style import to CSS and add confirm dialog styles with transitions

--- packages/utils/src/lib/index.js
+++ packages/utils/src/lib/index.js
@@ -1,5 +1,5 @@
 // Reexport your entry components here
-import './styles/app.scss';
+import './styles/app.css';
 export { hello } from './test.svelte.js';
 export { default as toast } from './toast.svelte.js';
 export { default as spinner } from './spinner.svelte.js';
