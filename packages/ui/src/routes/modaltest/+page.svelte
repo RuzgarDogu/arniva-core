4a82f3283be7fc8ca5e8a55c18f888c2f39d4a02
Emre Şahin <doguruzgar@gmail.com>
2025-04-05 11:08:19 +0300

    Bump package version to 0.0.76, update styles to use SCSS, and add modal test page with example usage

---
+++ packages/ui/src/routes/modaltest/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    /** @type {{ data: import('./$types').PageData }} */
+
+    import Wrapper from './Wrapper.svelte';
+    import { Button, Modal } from '$lib';
+    let { data } = $props();
+</script>
+
+<!-- <Wrapper /> -->
+
+<div style="height: 500px;">
+	<Button modal="testModal2" color="primary" style="margin-bottom: 20px;">Open Modal</Button>
+	<Modal 
+  devMode={false}
+  title="Working Example"
+  id="testModal2"
+  size="lg"
+>
+<Wrapper />
+
+	</Modal>
+</div>
---
+++ packages/ui/src/routes/modaltest/Wrapper.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { Button, Modal } from '$lib';
+</script>
+<div style="height: 500px;">
+
+    <h1>Modal Test</h1>
+	<Button modal="testModal" color="primary" style="margin-bottom: 20px;">Open Modal</Button>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam vero quibusdam delectus blanditiis error esse minima assumenda sit ratione veniam. Quae maiores dicta mollitia sunt et cumque iste, quas repellendus ratione perspiciatis amet.</p>
+	<Modal 
+  devMode={false}
+  title="Working Example"
+  id="testModal"
+  size="lg"
+>
+		<h3>Test Title</h3>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, beatae ipsa alias
+			repellendus impedit distinctio laboriosam, voluptatem reiciendis repellat perferendis sed
+			quo unde ratione quam consequuntur corporis molestiae. Magni veritatis, iusto harum
+			nostrum quasi aut minima quidem dolor placeat nulla saepe consequuntur quibusdam qui
+			blanditiis labore deleniti! Blanditiis, obcaecati. Nulla!
+		</p>
+		<Button color="danger" style="margin-bottom: 20px;">Go Somewhere</Button>
+	</Modal>
+</div>
