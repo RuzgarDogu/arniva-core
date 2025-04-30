7d661301343939fc7c5a9680ab36301df03b56f5
Emre Şahin <doguruzgar@gmail.com>
2025-04-30 15:13:08 +0300

    feat: Increment version to 0.1.06; enhance ListGroup component to support dynamic column layouts and add new ListGroup page

--- packages/ui/src/lib/general/ListGroup.svelte
+++ packages/ui/src/lib/general/ListGroup.svelte
@@ -1,14 +1,18 @@
 <script>
 	/**
 	 * @typedef {Object<string, any>} Props
+	 * @property {number} [columns] Number of columns (1-5)
 	 * @property {any} [children] ListGroupItem content
 	 * @property {any} rest ListGroupItem props
 	 */
 
 	/** @type {Props} */
-	let { children, ...rest } = $props();
+	let { columns = 1, children, ...rest } = $props();
+
+	// Ensure columns is within the range of 1 to 5
+	const columnsClass = `list-group--columns-${Math.min(Math.max(columns, 1), 5)}`;
 </script>
 
-<div class="list-group" {...rest}>
+<div class={`list-group ${columnsClass}`} {...rest}>
 	{@render children?.()}
 </div>
---
+++ packages/ui/src/routes/listgroup/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    /** @type {{ data: import('./$types').PageData }} */
+
+    import { ListGroup, ListGroupItem } from '$lib';
+
+    let { data } = $props();
+</script>
+
+<ListGroup columns={2}>
+	<ListGroupItem>
+		<p>List Item 1</p>
+	</ListGroupItem>
+	<ListGroupItem>
+		<p>List Item 2</p>
+	</ListGroupItem>
+	<ListGroupItem>
+		<p>List Item 3</p>
+	</ListGroupItem>
+	<ListGroupItem>
+		<p>List Item 4</p>
+	</ListGroupItem>
+	<ListGroupItem>
+		<p>List Item 5</p>
+	</ListGroupItem>
+</ListGroup>
